5f9129f0aecbf29080500d454247333f04de32f0
helenwbb <79942455+helenwbb@users.noreply.github.com>
2021-04-02 21:21:48 -0400

    updated css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 
 console.log("Welcome to My Portfilio!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className+='responsive';
+}else{
+  x.className='navtoggle';
+}
