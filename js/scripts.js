54876125abb6554dd484380fd4ae94f6160afc2b
helenwbb <79942455+helenwbb@users.noreply.github.com>
2021-04-04 12:02:18 -0400

    css and java
    fixed css brackets and added missing JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-
 console.log("Welcome to My Portfilio!")
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className+='responsive';
-}else{
-  x.className='navtoggle';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
